b24c7150e73241f915bf609e4f03db46d60a89c2
Chris Underwood <cunderwoodmn@gmail.com>
2020-06-25 12:44:57 -0400

    continued to add customizations to the index.js for scholarship_data

--- scholarship_data/index.js
+++ scholarship_data/index.js
@@ -1,1 +1,18 @@
+const fs = require('fs');
+const Crawler = require('crawler');
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
+// brings in the URLs to scrape
+const { urls } = require('../researcher_URLs/userURLs.js');
+
+// puts closing root tag on the document
+c.on('drain', function () {
+  fs.createWriteStream('./doi_researcher.csv', { flags: 'a' }).write(
+    'Do I need to put anything in here?'
+  );
+});
+
+// list of pages to scrape.
+c.queue(urls);
+
 encodeURI(uri);
