6baac29eda073b4899483b5cef88b910e1f14de5
Chris Underwood <cunderwoodmn@gmail.com>
2020-06-25 14:44:39 -0400

    add truncating o doi_researcher.csv to index.js

--- scholarship_data/index.js
+++ scholarship_data/index.js
@@ -3,10 +3,16 @@ const Crawler = require('crawler');
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 // brings in the URLs to scrape
-const { urls } = require('../researcher_URLs/userURLs.js');
+//const { urls } = require('../researcher_URLs/userURLs.js');
+
+// brings in the Testing URLs to scrape
+const { urls } = require('../researcher_URLs/userURLsTesting.js');
 
 // brings in user object for 'bad' user ids
 const { userIds } = require('../researcher_data/ProblemUIDs.js');
+
+// Truncate saved.xml before appending
+fs.truncateSync('./doi_researcher.csv');
 
 // Place reserved for functions to remove code jobs.
 
@@ -25,7 +31,7 @@ const c = new Crawler({
       xmlMode: true,
     },
   },
-
+  // This will be called for each crawled page
   callback: async function (error, res, done) {
     if (error) {
       console.log(error);
@@ -35,6 +41,23 @@ const c = new Crawler({
       let $ = res.$;
       // beginning of code scrape each section of a researcher
       let con = '';
+
+      // Alma Primary ID
+      let email = await $('div#contact > a').text();
+      let email0 = email.split('@')[0];
+      let email1 = email.split('@')[1];
+
+      if (email1 !== 'brandeis.edu') {
+        con += 'unknown_Alma_ID';
+      } else {
+        let newId = userIds[email0];
+
+        if (newId !== undefined) {
+          email0 = newId;
+        }
+
+        console.log(email0, email1, email);
+      }
 
       // writes each user to our file.
       fs.createWriteStream('./doi_researcher.csv', { flags: 'a' }).write(con);
