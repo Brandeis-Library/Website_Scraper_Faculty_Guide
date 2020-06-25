59ae1fb30c187b7c4f49c5d568bab434b45ca81c
Chris Underwood <cunderwoodmn@gmail.com>
2020-06-25 14:02:55 -0400

    basic scraper set up. Next step is to add lines to print out.

--- scholarship_data/index.js
+++ scholarship_data/index.js
@@ -5,6 +5,46 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 // brings in the URLs to scrape
 const { urls } = require('../researcher_URLs/userURLs.js');
 
+// brings in user object for 'bad' user ids
+const { userIds } = require('./ProblemUIDs.js');
+
+// Place reserved for functions to remove code jobs.
+
+// starts CSV file with column headings.
+fs.createWriteStream('./doi_researcher.csv', { flags: 'a' }).write(
+  `ALAMID, DOI`
+);
+
+// beginning of scrapping function.
+const c = new Crawler({
+  maxConnections: 1,
+  jQuery: {
+    name: 'cheerio',
+    options: {
+      normalizeWhitespace: true,
+      xmlMode: true,
+    },
+  },
+
+  callback: async function (error, res, done) {
+    if (error) {
+      console.log(error);
+    } else {
+      // $ is Cheerio by default
+      //$ is a lean implementation of core jQuery designed specifically for the server
+      let $ = res.$;
+      // beginning of code scrape each section of a researcher
+      let con = '';
+
+      // writes each user to our file.
+      fs.createWriteStream('./doi_researcher.csv', { flags: 'a' }).write(con);
+
+      // signifies the end of each researcher being scraped
+      done();
+    }
+  },
+});
+
 // puts closing root tag on the document
 c.on('drain', function () {
   fs.createWriteStream('./doi_researcher.csv', { flags: 'a' }).write(
