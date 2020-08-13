e1886125c88dd1d504b3e59d0da91704853a761d
Chris Underwood <cunderwoodmn@gmail.com>
2020-08-13 13:07:56 -0400

    current list of Faculty Guide pages that are being used for testing the scraping functionality.

--- researcher_URLs/userURLsTesting.js
+++ researcher_URLs/userURLsTesting.js
@@ -2,6 +2,11 @@ module.exports = {
   urls: [
     'https://www.brandeis.edu/facultyguide/person.html?emplid=2ff4bc1096fde4d54ce6b5bb8f327adc3cf7f704',
     'https://www.brandeis.edu/facultyguide/person.html?emplid=797e68e246969fbd1b40828bafd1c07825056b6a',
-    //'https://www.brandeis.edu/facultyguide/person.html?emplid=4a09ce0d1a4b429db8f07e9e0210e5e2e7ab094b',
+    'https://www.brandeis.edu/facultyguide/person.html?emplid=4a09ce0d1a4b429db8f07e9e0210e5e2e7ab094b',
+    'https://www.brandeis.edu/facultyguide/person.html?emplid=ea7626a141dae075fc17cc3b9bdf20ce95d7d8fa',
+    'https://www.brandeis.edu/facultyguide/person.html?emplid=6d9e892986923a82f4a14a818f7c970060c80ff8',
+    'https://www.brandeis.edu/facultyguide/person.html?emplid=9fb3674dcbdcd262263cf7b5bd987c45e0a4c10a',
+    'https://www.brandeis.edu/facultyguide/person.html?emplid=4dd233275d124d9d0d17eda2d59c0b0e1c158b5e',
+    'https://www.brandeis.edu/facultyguide/person.html?emplid=9d6e7a17246d137200237a48208b32ed6082d0d8',
   ],
 };
