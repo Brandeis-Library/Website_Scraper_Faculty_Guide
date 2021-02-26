5d155229774957c27496c0836f52cd54553aa6e8
Chris Underwood <cunderwoodmn@gmail.com>
2021-02-25 20:41:52 -0800

    fixed data in researcherAffil.csv so that it can be read most efficiently.

--- updating_researchers/index4.js
+++ updating_researchers/index4.js
@@ -4,11 +4,17 @@ const XLSX = require('xlsx');
 // brings in the spreadsheet obs + faculty guide scraped data
 const { userEnhancedObjs } = require('./Spreadsheet_Objs_Plus_Fac_Guide.js');
 
-// brings in the ExLibris Analytics spreadsheet of Internal Affiliations positions and titles
-const { researchAffil } = require('./researcherAffil.csv');
-
 //console.log('userEnhancedObjs----- ', userEnhancedObjs);
-//console.log('researchAffil----- ', researchAffil);
-setTimeout(function () {
-  console.log('researchAffil----- ', researchAffil);
-}, 3000);
+
+// brings in the ExLibris Analytics spreadsheet of Internal Affiliations positions and titles
+
+const workbook = XLSX.readFile('researcherAffil.csv');
+
+//console.log('workbook----- ', workbook);
+
+const sheetNames = workbook.SheetNames;
+console.log('sheetNames -----', sheetNames);
+const sheetIndex = 1;
+
+var df = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNames[sheetIndex - 1]]);
+console.table(df);
