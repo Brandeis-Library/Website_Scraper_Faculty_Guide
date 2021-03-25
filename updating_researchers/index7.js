f31202b953825864b1a4aeba74973cc9cf9b59e4
Chris Underwood <cunderwoodmn@gmail.com>
2021-03-25 15:10:38 -0700

    add professor_practice and reorganized the order of the if entries to make sure they are grabbed in the proper order

--- updating_researchers/index7.js
+++ updating_researchers/index7.js
@@ -38,58 +38,29 @@ const fs = require('fs');
 
       // map title from Workday to codes for postion in Esploro
       //  user.titleWorkday === 'Professor' user.titleWorkday === '' ||
-      if (
-        user.titleWorkday === 'Professor' ||
-        user.titleWorkday === 'Professor of Computer Science' ||
-        user.titleWorkday === 'Professor of Mathematics' ||
-        user.titleWorkday === 'Professor of Psychology' ||
-        user.titleWorkday === 'Professor of Physics' ||
-        user.titleWorkday === 'Professor of Chemistry' ||
-        user.titleWorkday === 'Professor of Theater Arts' ||
-        user.titleWorkday ===
-          'Professor of Biology and Harold and Bernice Davis Chair in Aging and Neurodegenerative Disease'
-      ) {
-        positionVar = 'professor';
-      } else if (user.titleWorkday === 'Postdoctoral Fellow') {
-        positionVar = 'postdoctoral_fellow';
-      } else if (
-        user.titleWorkday === 'Assistant Professor of English' ||
-        user.titleWorkday === 'Assistant Professor of Philosophy' ||
-        user.titleWorkday ===
-          'Assistant Professor in Education and Studies in Jewish Education' ||
-        user.titleWorkday === 'Assistant Professor of Economics' ||
-        user.titleWorkday === 'Assistant Professor of Psychology' ||
-        user.titleWorkday ===
-          'Assistant Professor of Education and Director of Teacher Education' ||
-        user.titleWorkday === 'Assistant Professor of Chemistry' ||
-        user.titleWorkday === 'Assistant Professor of Chemistry' ||
-        user.titleWorkday ===
-          'Assistant Professor of University Writing and Director of First-Year Writing' ||
-        user.titleWorkday === 'Assistant Professor of Physics' ||
-        user.titleWorkday === 'Assistant Research Professor of Physics' ||
-        user.titleWorkday ===
-          'Assistant Professor of Education and Sociology' ||
-        user.titleWorkday === 'Assistant Professor of Politics' ||
-        user.titleWorkday === 'Assistant Professor in Psychology' ||
-        user.titleWorkday === 'Assistant Professor of Computer Science' ||
-        user.titleWorkday === 'Assistant Professor'
-      ) {
-        positionVar = 'assistant_professor';
-      } else if (user.titleWorkday.startsWith('Lecturer')) {
-        positionVar = 'lecturer';
+      if (user.titleWorkday.startsWith('Professor of the Practice')) {
+        positionVar = 'professor_practice';
       } else if (
         user.titleWorkday.startsWith('Associate Professor of the Practice')
       ) {
         positionVar = 'associate_practice';
+      } else if (user.titleWorkday === 'Postdoctoral Fellow') {
+        positionVar = 'postdoctoral_fellow';
+      } else if (user.titleWorkday.startsWith('Assistant Professor')) {
+        positionVar = 'assistant_professor';
+      } else if (user.titleWorkday.startsWith('Lecturer')) {
+        positionVar = 'lecturer';
       } else if (user.titleWorkday.startsWith('Associate Professor')) {
         positionVar = 'associate_professor';
+      } else if (user.titleWorkday.startsWith('Professor')) {
+        positionVar = 'professor';
       } else {
         // Can use this to test if any are in the category with a long\specific text
         positionVar = user.titleWorkday;
       }
 
       console.log('unet', unet);
-      let str = `<user><primary_id>${user.unet}</primary_id><is_researcher>true</is_researcher><researcher><researcher_first_name>${user.firstName}</researcher_first_name><researcher_last_name>${user.lastName}></researcher_last_name><position>${positionVar}</position>${displayTitleVar}<researcher_organization_affiliations>${costCentersVar}</researcher_organization_affiliations></researcher></user>`;
+      let str = `<user><primary_id>${user.unet}</primary_id><is_researcher>true</is_researcher><researcher><researcher_first_name>${user.firstName}</researcher_first_name><researcher_last_name>${user.lastName}</researcher_last_name><position>${positionVar}</position>${displayTitleVar}<researcher_organization_affiliations>${costCentersVar}</researcher_organization_affiliations></researcher></user>`;
       console.log('for in loop', str);
       await fs
         .createWriteStream('./updateRearcher.xml', { flags: 'a' })
